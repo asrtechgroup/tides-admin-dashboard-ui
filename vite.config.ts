d53a4439320c4e0ef92cbc90271f7c64c8e29c6a
asrtechgroup <asrtech01company@gmail.com>
2025-07-11 19:56:02 +0300

    Deploy to Render

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/tides-admin-dashboard-ui/', // Set base for GitHub Pages
   server: {
     host: "::",
     port: 8080,
